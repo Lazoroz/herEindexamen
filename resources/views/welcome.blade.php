e16d30dffe2bb0fdcbdcccc7b8c823a4eede9c1f
Lazoroz <ramlal524@gmail.com>
2023-06-01 16:03:17 +0200

    aa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,17 +17,17 @@
 <body>
     <section class="w-full h-screen bg transition-opacity">
 
-            <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">                   
-                    <a href="{{ route('login') }}" class="font-cormorant text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+        <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right absolute z-10">                   
+            <a href="{{ route('login') }}" class="font-cormorant text-white hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+        </div>
+        {{-- <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">                   
+            <a href="{{ route('register') }}" class="font-cormorant text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Inschrijven</a>
+        </div> --}}
 
-                    <a href="{{ route('/') }}">Inschrijven</a>
-            </div>
-
-        <img
-          src="https://wallpaperaccess.com/full/1271276.jpg"
-          class="w-full"
-          alt="Image alt text"
-        />
+        <div
+          class="w-full h-full bg-cover"
+          style="background: url({{URL('images/background-tennis.jpeg')}});" 
+        ></div>
     </section>
 
     
